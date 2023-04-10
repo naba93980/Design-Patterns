c3cf1a391642d2d7b69c6162a827cc075b0efaca
NabajyotiModak <naba93981@gmail.com>
2023-04-10 22:10:56 +0530

    book class - details of book

---
+++ Creational/PrototypePattern/com/prototype/Book.java
@@ -0,0 +1,23 @@
+package com.prototype;
+
+public class Book {
+    private int bid;
+    private String bookName;
+    public int getBid() {
+        return bid;
+    }
+    public void setBid(int bid) {
+        this.bid = bid;
+    }
+    public String getBookName() {
+        return bookName;
+    }
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+    @Override
+    public String toString() {
+        return "Book [bid=" + bid + ", bookName=" + bookName + "]";
+    }
+    
+}
